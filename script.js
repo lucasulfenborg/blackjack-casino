35d3fc5804a94ae81b68422619c4ce496d022d2d
Lucas Ulfenborg <lucasulfenborg@gmail.com>
2024-06-03 21:30:46 +0200

    asda

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ class player {
                 this.money -= amount;
             }
     }
+
 }
 
 class game {
@@ -25,6 +26,11 @@ class game {
         this.name = name;
         this.players = [];
     }
+
+    static delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
 
     addPlayer(player) {
         this.players.push(player);
@@ -77,12 +83,27 @@ class game {
 
         let betAmount = document.createElement('span');
         betAmount.classList.add('betAmount');
-        betAmount.innerHTML = 'Current bet: ' + this.bets;
+        betAmount.innerHTML = 'Current bet: ' + this.bet;
         infoDiv.appendChild(betAmount);
+      
+    }
 
-        
+    CreateGameScreen() {
+        let gameScreen = document.createElement('div');
+        gameScreen.classList.add('gameScreen');
+        document.body.appendChild(gameScreen);
+    }
 
 
+    createNewGameButton() { //inside actionContainer
+        let actionContainer = document.getElementsByClassName('actionContainer')[0];
+        let newGameButton = document.createElement('button');
+        newGameButton.classList.add('newGameButton');
+        newGameButton.innerHTML = 'New Game';
+        newGameButton.addEventListener('click', function() {
+            this.startGame();
+        }.bind(this));
+        actionContainer.appendChild(newGameButton);
     }
 
 
@@ -91,7 +112,7 @@ class game {
 
         let actionContainer = document.getElementsByClassName('actionContainer')[0];
         let betAmount = document.getElementsByClassName('betAmount')[0];
-        betAmount.innerHTML = 'Current bet: ' + this.bets;
+        betAmount.innerHTML = 'Current bet: ' + this.bet;
         
 
         //Display div containing bettings (5, 10, 20)
@@ -123,8 +144,8 @@ class game {
                     return;
                 }
                 else {
-                    this.bets += parseInt(button.innerHTML);
-                    betAmount.innerHTML = 'Current bet: ' + this.bets;
+                    this.bet += parseInt(button.innerHTML);
+                    betAmount.innerHTML = 'Current bet: ' + this.bet;
                     player1.removeMoney(parseInt(button.innerHTML));
                     document.getElementsByClassName('wallet')[0].innerHTML = 'Wallet: ' + player1.money;
                 }
@@ -155,19 +176,72 @@ class game {
 
 
 class pokerGame extends game {
+    constructor() {
+        super();
+        this.deck = [];
+    }
+
+
     createDeck() { //Standard poker cards
-        let deck = ['h1','h2','h3','h4','h5','h6','h7','h8','h9','h10','h11','h12','h13',
+        this.deck = ['h1','h2','h3','h4','h5','h6','h7','h8','h9','h10','h11','h12','h13',
                     'd1','d2','d3','d4','d5','d6','d7','d8','d9','d10','d11','d12','d13',
                     'c1','c2','c3','c4','c5','c6','c7','c8','c9','c10','c11','c12','c13',
                     's1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13'];                
-        return deck;
     }
 
-    dealCard(deck) {
-        let card = deck[Math.floor(Math.random() * deck.length)];
-        deck.splice(deck.indexOf(card), 1);
+    dealCard() {
+        let card = this.deck[Math.floor(Math.random() * this.deck.length)];
+        this.deck.splice(this.deck.indexOf(card), 1);
         return card;
     }
+
+    CreateCardsDisplay() {
+        //add cards to gamescreen
+        let gameScreen = document.getElementsByClassName('gameScreen')[0];
+        
+        let player_cards_element = document.createElement('div');
+        let dealer_cards_element = document.createElement('div');
+        
+        dealer_cards_element.classList.add('dealer_cards');
+        player_cards_element.classList.add('player_cards');
+    
+        gameScreen.appendChild(dealer_cards_element);
+        gameScreen.appendChild(player_cards_element);
+
+    }
+
+    updateCardsDisplay(player_cards, dealer_cards) {
+        let player_cards_element = document.getElementsByClassName('player_cards')[0];
+        let dealer_cards_element = document.getElementsByClassName('dealer_cards')[0];
+
+        //remove all cards
+        player_cards_element.innerHTML = '';
+        dealer_cards_element.innerHTML = '';
+
+        for (let card of player_cards) {
+            let player_card = document.createElement('div');
+            player_card.innerHTML = card;
+            player_card.classList.add('card');
+            player_cards_element.appendChild(player_card);
+        }
+
+        for (let card of dealer_cards) {
+            let dealer_card = document.createElement('div');
+            dealer_card.innerHTML = card;
+            dealer_card.classList.add('card');
+            dealer_cards_element.appendChild(dealer_card);
+        }
+
+     
+
+        //player_cards_element.innerHTML = player_cards.join(' ');
+        //dealer_cards_element.innerHTML = dealer_cards.join(' ');
+
+
+
+
+    }
+
 
     hitOrStand() {
         return new Promise((resolve, reject) => {
@@ -211,40 +285,33 @@ class blackJack extends pokerGame {
         super();
         this.current_Player_Index = 0;
         this.name = "BlackJack";
+
         
     }
 
     calculateHandValue(cards) {
         let value = 0;
-        let ace = 0;
+        let aces = 0;
+    
         for (let card of cards) {
-            if (card[0] == 'h' || card[0] == 'd' || card[0] == 'c') {
-                if (card[1] == '1' || card[1] == '11' || card[1] == '12' || card[1] == '13') { //Kings, queens and jacks are worth 10
-                    value += 10;
-                }
-                else {
-                    value += parseInt(card[1]);
-                }
-            }
-            else {
-                if (card[1] == '1') {
-                    ace++;
-                }
-                else {
-                    value += parseInt(card[1]);
-                }
-            }
-        }
-        for (let i = 0; i < ace; i++) { //Ace worth either 1 or 11
-            if (value + 11 <= 21) {
+            let cardValue = parseInt(card.substring(1));
+            if (cardValue > 10) { // face card
+                value += 10;
+            } else if (cardValue === 1) { // Ace
                 value += 11;
-            }
-            else {
-                value += 1;
+                aces += 1;
+            } else {
+                value += cardValue;
             }
         }
+    
+        // If value is over 21 and there's an ace counted as 11, reduce the value
+        while (value > 21 && aces > 0) {
+            value -= 10;
+            aces -= 1;
+        }
+    
         return value;
-
     }
 
     async startGame() {
@@ -252,75 +319,129 @@ class blackJack extends pokerGame {
         document.body.innerHTML = '';
 
         this.player_cards = [];
-        this.dealer_Cards = [];
-        this.deck = this.createDeck();
-        this.bets = 0;
+        this.dealer_cards = [];
+        this.createDeck();
+        this.bet = 0;
+        let playerValue = 0;
+        let dealerValue = 0;
+        let payout = 0;
+        let betAmount = this.bet;
+        let end_message = "...";
+        let game_state_element = document.getElementsByClassName('gameState')[0];
+        let player = this.players[this.current_Player_Index];
 
+        this.CreateGameScreen();
+        this.CreateCardsDisplay();
         this.createActionContainer();
 
         //ask for bet
         await this.askForBet();
 
         // deal first card to player
-        this.player_cards.push(this.dealCard(this.deck));
-        console.log("Your cards: ", this.player_cards);
+        await game.delay(500);
+        this.player_cards.push(this.dealCard());
+        this.updateCardsDisplay(this.player_cards, this.dealer_cards);
+
 
         // Deal first card to dealer
-        this.dealer_Cards.push(this.dealCard(this.deck));
-        console.log("Dealer cards: ", this.dealer_Cards);
+        await game.delay(500);
+        this.dealer_cards.push(this.dealCard());
+        this.updateCardsDisplay(this.player_cards, this.dealer_cards);
+
 
         //display cards
 
         // ask for decision (hit or stand) untill stand or bust
-        while(true) {
+        let continueGame = true;
+        while(continueGame) {
             let choice = await this.hitOrStand();
             switch(choice) {
                 case 'hit':
-                    this.player_cards.push(this.dealCard(this.deck));
-                    console.log("Your cards: ", this.player_cards);
+                    await game.delay(1000);
+                    this.player_cards.push(this.dealCard());
+                    this.updateCardsDisplay(this.player_cards, this.dealer_cards);
                     if (this.calculateHandValue(this.player_cards) > 21) {
-                        console.log("You busted! Dealer wins!");
-                        break;
+                        end_message = "You busted! Dealer wins!";
+                        continueGame = false;
                     }
+                    break;
 
                 case 'stand':
+                    continueGame = false;
                     break;
             }
-            break
+  
+       
         }
         // deal cards to dealer
-        while(this.calculateHandValue(this.dealer_Cards) < 17) {
-            this.dealer_Cards.push(this.dealCard(this.deck));
-            console.log("Dealer cards: ", this.dealer_Cards);
+        while(this.calculateHandValue(this.dealer_cards) < 17) {
+            this.dealer_cards.push(this.dealCard());
+            this.updateCardsDisplay(this.player_cards, this.dealer_cards);
+            await game.delay(1000);
+
+
         }
         // determine winner
-        let playerValue = this.calculateHandValue(this.player_cards);
-        let dealerValue = this.calculateHandValue(this.dealer_Cards);
+        playerValue = this.calculateHandValue(this.player_cards);
+        dealerValue = this.calculateHandValue(this.dealer_cards);
+
         switch(true) {
+            case playerValue == 21 && this.player_cards.length == 2:
+                end_message = "You got a blackjack! You win";
+                payout = betAmount * 2.5;
+                break
+            case dealerValue == 21 && this.dealer_cards.length == 2:
+                end_message = "Dealer got a blackjack! Dealer wins!";
+                payout = 0;
+                break;
             case dealerValue > 21:
-                console.log("Dealer busted! You win!");
+                end_message = "Dealer busted! You win!";
+                payout = betAmount * 2;
                 break;
             case playerValue > dealerValue:
-                console.log("You win!");
+                payout = betAmount * 2;
+                end_message = "You win!";
                 break;
             case playerValue < dealerValue:
-                console.log("Dealer wins!");
+                end_message = "Dealer wins!";
+                payout = 0;
                 break;
             case playerValue == dealerValue:
-                console.log("Push");
+                end_message = "Push";
+                payout = betAmount;
                 break;
         }
 
+        player1.addMoney(payout);
+        document.getElementsByClassName('wallet')[0].innerHTML = 'Wallet: ' + player1.money;
+        document.getElementsByClassName('gameState')[0].innerHTML = end_message;
 
+        //remove buttons
+        document.getElementsByClassName('hitButton')[0].remove();
+        document.getElementsByClassName('standButton')[0].remove();
+        this.createNewGameButton();
 
+        
     }
 }
 
+class blackJackWithPush extends blackJack {
+    constructor() {
+        super();
+        this.name = "BlackJack with push";
+    }
+
+    //Override
+    
+
+}
+
+
 let games = [
-    blackJack1 = new blackJack(),
+    new blackJack(),
 ];
 
-player1 = new player('Kalle', 100);
+let player1 = new player('Spelare1', 1000);
 
 for (let game of games) {
     game.createGameIcon();
