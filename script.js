d5d6d47b4d10119339419dee72571b146d1de285
Lucas Ulfenborg <lucasulfenborg@gmail.com>
2024-06-04 21:54:24 +0200

    final

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// ha understreck i variabel_namn
-// Metoder och klasser ska vara i camelCase
+//variable names should be in snake_case
+//Methods and classes should be in camelCase
 
 class player {
+    #money;
+
     constructor(name, money) {
         this.name = name;
         this.money = money;
@@ -19,12 +21,17 @@ class player {
             }
     }
 
+    getMoney() {
+        return this.#money;
+    }
+
 }
 
 
 class game {
-    constructor(name) {
+    constructor(name, main) {
         this.name = name;
+        this.main = main;
         this.players = [];
     }
 
@@ -41,14 +48,12 @@ class game {
         this.players.splice(this.players.indexOf(player), 1);
     }
 
-    //Method to create a html "card" element for the game
     createGameIcon() {
         let cardElement = document.createElement('div');
         cardElement.classList.add('gameIcon');
         cardElement.innerHTML = this.name;
         document.body.appendChild(cardElement);
 
-        //make game icon clickable that runs startGame()
         cardElement.addEventListener('click', function() {
             this.startGame();
         }.bind(this));
@@ -57,6 +62,13 @@ class game {
     }
 
     startGame() {
+        document.body.innerHTML = '';
+        document.title = this.name;
+
+        this.createActionContainer();
+
+        this.CreateGameScreen();
+
 
     }
 
@@ -70,22 +82,24 @@ class game {
         infoDiv.classList.add('infoDiv');
         actionContainer.appendChild(infoDiv);
 
-        //Display info above the div, wallet and current bet
+        //Display info above 
         let wallet = document.createElement('span');
         wallet.classList.add('wallet');
-        wallet.innerHTML = 'Wallet: ' + Main.player1.money;
+        wallet.innerHTML = 'Wallet: ' + this.main.player1.getMoney();
         infoDiv.appendChild(wallet);
 
         //Display current state of game
         let gameState = document.createElement('div');
         gameState.classList.add('gameState');
-        gameState.innerHTML = 'Place your bets...';
+        gameState.innerHTML = '...';
         infoDiv.appendChild(gameState);
 
-        let betAmount = document.createElement('span');
-        betAmount.classList.add('betAmount');
-        betAmount.innerHTML = 'Current bet: ' + this.bet;
-        infoDiv.appendChild(betAmount);
+        let bet_amount = document.createElement('span');
+        bet_amount.classList.add('bet_amount');
+        bet_amount.innerHTML = 'Current bet: ' + this.bet;
+        infoDiv.appendChild(bet_amount);
+
+        
       
     }
 
@@ -107,31 +121,43 @@ class game {
         actionContainer.appendChild(newGameButton);
     }
 
+    createHomeButton() {
+        let actionContainer = document.getElementsByClassName('actionContainer')[0];
+        let homeButton = document.createElement('button');
+        homeButton.classList.add('homeButton');
+        homeButton.innerHTML = 'Home';
+        homeButton.addEventListener('click', function() {
+            this.main.createMainMenu();
+        }.bind(this));
+        actionContainer.appendChild(homeButton);
+    }
+    
+
 
     askForBet() {
         return new Promise((resolve, reject) => {
 
         let actionContainer = document.getElementsByClassName('actionContainer')[0];
-        let betAmount = document.getElementsByClassName('betAmount')[0];
-        betAmount.innerHTML = 'Current bet: ' + this.bet;
+        let bet_amount = document.getElementsByClassName('bet_amount')[0];
+        bet_amount.innerHTML = 'Current bet: ' + this.bet;
         
 
-        //Display div containing bettings (5, 10, 20)
-        let bet5 = document.createElement('button');
-        bet5.classList.add('betButton');
-        bet5.innerHTML = '5';
-        actionContainer.appendChild(bet5);
+        //Display div containing bets (5, 10, 20)
+        let bet25 = document.createElement('button');
+        bet25.classList.add('betButton', 'bet25');
+        bet25.innerHTML = '25';
+        actionContainer.appendChild(bet25);
 
-        let bet10 = document.createElement('button');
-        bet10.innerHTML = '10';
-        bet10.classList.add('betButton');
-        actionContainer.appendChild(bet10);
+        let bet100 = document.createElement('button');
+        bet100.innerHTML = '100';
+        bet100.classList.add('betButton', 'bet100');
+        actionContainer.appendChild(bet100);
 
         
-        let bet20 = document.createElement('button');
-        bet20.classList.add('betButton');
-        bet20.innerHTML = '20';
-        actionContainer.appendChild(bet20);
+        let bet500 = document.createElement('button');
+        bet500.classList.add('betButton', 'bet500');
+        bet500.innerHTML = '500';
+        actionContainer.appendChild(bet500);
 
         let doneButton = document.createElement('button');
         doneButton.classList.add('doneButton');
@@ -141,14 +167,14 @@ class game {
         //Eventlisteners for betting
         for (let button of document.getElementsByClassName('betButton')) {
             button.addEventListener('click', function() {
-                if (Main.player1.money < parseInt(button.innerHTML)) {
+                if (this.main.player1.money < parseInt(button.innerHTML)) {
                     return;
                 }
                 else {
                     this.bet += parseInt(button.innerHTML);
-                    betAmount.innerHTML = 'Current bet: ' + this.bet;
-                    Main.player1.removeMoney(parseInt(button.innerHTML));
-                    document.getElementsByClassName('wallet')[0].innerHTML = 'Wallet: ' + Main.player1.money;
+                    bet_amount.innerHTML = 'Current bet: ' + this.bet;
+                    this.main.player1.removeMoney(parseInt(button.innerHTML));
+                    document.getElementsByClassName('wallet')[0].innerHTML = 'Wallet: ' + this.main.player1.money;
                 }
             }.bind(this));
         }
@@ -177,8 +203,8 @@ class game {
 
 
 class pokerGame extends game {
-    constructor() {
-        super();
+    constructor(name, main) {
+        super(name, main);
         this.deck = [];
     }
 
@@ -197,7 +223,6 @@ class pokerGame extends game {
     }
 
     CreateCardsDisplay() {
-        //add cards to gamescreen
         let gameScreen = document.getElementsByClassName('gameScreen')[0];
         
         let player_cards_element = document.createElement('div');
@@ -234,12 +259,6 @@ class pokerGame extends game {
             dealer_cards_element.appendChild(dealer_card);
 
         }
-
-     
-
-        //player_cards_element.innerHTML = player_cards.join(' ');
-        //dealer_cards_element.innerHTML = dealer_cards.join(' ');
-
 
 
 
@@ -284,8 +303,8 @@ class pokerGame extends game {
 }
 
 class blackJack extends pokerGame {
-    constructor() {
-        super();
+    constructor(main) {
+        super("BlackJack", main);
         this.current_Player_Index = 0;
         this.name = "BlackJack";
 
@@ -297,14 +316,14 @@ class blackJack extends pokerGame {
         let aces = 0;
     
         for (let card of cards) {
-            let cardValue = (card.substring(1));
-            if (cardValue === 'J' || cardValue === 'Q' || cardValue === 'K') { // face card
+            let card_value = (card.substring(1));
+            if (card_value === 'J' || card_value === 'Q' || card_value === 'K') { // face card
                 value += 10;
-            } else if (cardValue === "A") { // Ace
+            } else if (card_value === "A") { // Ace
                 value += 11;
                 aces += 1;
             } else {
-                value += parseInt(cardValue);
+                value += parseInt(card_value);
             }
         }
     
@@ -314,36 +333,46 @@ class blackJack extends pokerGame {
             aces -= 1;
         }
         
-        console.log(value)
         return value;
 
     }
 
     async startGame() {
         //Clear
-        document.body.innerHTML = '';
+
+        super.startGame();
+        
 
         this.player_cards = [];
         this.dealer_cards = [];
         this.createDeck();
         this.bet = 0;
-        let playerValue = 0;
-        let dealerValue = 0;
+
+        let player_value = 0;
+        let dealer_value = 0;
         let payout = 0;
-        let betAmount = this.bet;
+        let bet_amount = this.bet;
         let end_message = "...";
         let game_state_element = document.getElementsByClassName('gameState')[0];
         let player = this.players[this.current_Player_Index];
 
-        this.CreateGameScreen();
+
         this.CreateCardsDisplay();
-        this.createActionContainer();
+
+        let wallet_element = document.getElementsByClassName('wallet')[0];
+        wallet_element.innerHTML = 'Wallet: ' + this.main.player1.money;
+        let game_state = document.getElementsByClassName('gameState')[0];
+        
+        game_state.innerHTML = 'Place your bet!';
 
         //ask for bet
         await this.askForBet();
-        betAmount = this.bet;
+        bet_amount = this.bet;
 
-        // deal first card to player
+        // deal cards
+        await game.delay(500);
+        this.player_cards.push(this.dealCard());
+        this.updateCardsDisplay(this.player_cards, this.dealer_cards);
         await game.delay(500);
         this.player_cards.push(this.dealCard());
         this.updateCardsDisplay(this.player_cards, this.dealer_cards);
@@ -357,7 +386,7 @@ class blackJack extends pokerGame {
 
         //display cards
 
-        // ask for decision (hit or stand) untill stand or bust
+        //ask for decision (hit or stand) until stand or bust
         let continueGame = true;
         while(continueGame) {
             let choice = await this.hitOrStand();
@@ -378,7 +407,7 @@ class blackJack extends pokerGame {
   
        
         }
-        // deal cards to dealer
+        //deal cards to dealer
         while(this.calculateHandValue(this.dealer_cards) < 17) {
             this.dealer_cards.push(this.dealCard());
             this.updateCardsDisplay(this.player_cards, this.dealer_cards);
@@ -386,52 +415,53 @@ class blackJack extends pokerGame {
 
 
         }
-        // determine winner
-        playerValue = this.calculateHandValue(this.player_cards);
-        dealerValue = this.calculateHandValue(this.dealer_cards);
+        //determine winner
+        player_value = this.calculateHandValue(this.player_cards);
+        dealer_value = this.calculateHandValue(this.dealer_cards);
 
         switch(true) {
-            case playerValue > 21:
+            case player_value > 21:
                 end_message = "You busted! Dealer wins!";
                 payout = 0;
                 break;
 
-            case playerValue == dealerValue:
+            case player_value == dealer_value:
                 end_message = "Push";
-                payout = betAmount;
+                payout = bet_amount;
                 break;
             
-            case playerValue == 21 && this.player_cards.length == 2:
+            case player_value == 21 && this.player_cards.length == 2:
                 end_message = "You got a blackjack! You win";
-                payout = betAmount * 2.5;
+                payout = bet_amount * 2.5;
                 break
-            case dealerValue == 21 && this.dealer_cards.length == 2:
+            case dealer_value == 21 && this.dealer_cards.length == 2:
                 end_message = "Dealer got a blackjack! Dealer wins!";
                 payout = 0;
                 break;
-            case dealerValue > 21:
+            case dealer_value > 21:
                 end_message = "Dealer busted! You win!";
-                payout = betAmount * 2;
+                payout = bet_amount * 2;
                 break;
-            case playerValue > dealerValue:
-                payout = betAmount * 2;
+            case player_value > dealer_value:
+                payout = bet_amount * 2;
                 end_message = "You win!";
                 break;
-            case playerValue < dealerValue:
+            case player_value < dealer_value:
                 end_message = "Dealer wins!";
                 payout = 0;
                 break;
             
         }
 
-        Main.player1.addMoney(payout);
-        document.getElementsByClassName('wallet')[0].innerHTML = 'Wallet: ' + Main.player1.money;
-        document.getElementsByClassName('gameState')[0].innerHTML = end_message;
+        this.main.player1.addMoney(payout);
+        wallet_element.innerHTML = 'Wallet: ' + this.main.player1.money;
+        game_state_element.innerHTML = end_message;
 
         //remove buttons
         document.getElementsByClassName('hitButton')[0].remove();
         document.getElementsByClassName('standButton')[0].remove();
         this.createNewGameButton();
+        this.createHomeButton();
 
         
     }
@@ -440,16 +470,14 @@ class blackJack extends pokerGame {
 class Main {
     constructor() {
         this.games = [];
+        this.player1 = null;
     }
 
         setup () {
-
-            let player1 = new player('Spelare1', 1000);
+            this.player1 = new player('Player1', 100);
 
             this.games = [
-                new blackJack(),
-                new pokerGame(),
-                new blackJack(),
+                new blackJack(this),
             ];
 
             this.createMainMenu();
@@ -459,14 +487,21 @@ class Main {
 
         createMainMenu () {
             document.body.innerHTML = '';
+            //title
+            document.title = 'Casino';
 
-            // Casino title
+            //Casino title
             let title = document.createElement('h1');
-            title.innerHTML = 'Welcome to the Casino';
+            title.innerHTML = 'Welcome';
             document.body.appendChild(title);
 
-            
+            //select game
+            let selectgame = document.createElement('h2');
+            selectgame.innerHTML = 'Select your game of choice';
+            document.body.appendChild(selectgame);
 
+            
+            //game icons
             for (let game of this.games) {
                 game.createGameIcon();
         }
@@ -478,6 +513,4 @@ class Main {
 let main = new Main();
 main.setup();
 
-//blackJack1.addPlayer(player1);
-//blackJack1.startGame();
 
